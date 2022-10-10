be2e9a18cad7e630d08c7d5fa7f87336611bc31d
IsmaelMassolo <ismaelmassolo@gmail.com>
2022-10-10 19:11:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*Escriba por pantalla los pasos en secuencia para preparar una taza de café. Por cada paso
+ se debe esperar que el usuario presiones una tecla. Limpiar la pantalla al comenzar el programa.*/
+ 
+using System;
+namespace C_
+{
+    class Program
+    {static void Main(string[] args)
+        {
+            Console.WriteLine("Preparemos una taza de café! Estos son los pasos:");
+            Console.WriteLine("1- Poner agua en la pava");
+            Console.WriteLine("2- Prender la hornalla de la cocina y colocar la pava sobre ella");
+            Console.WriteLine("3- En una taza de cafe colocar dos cucharaditas de café y azucar a gusto");
+            Console.WriteLine("4- Una vez hervida el agua servir en la taza");
+            Console.WriteLine("5- Revolver y disfrutar");
+        }
+    }
+}
